a86f5808ba767e0899f7d535ac97b5bff55e98f1
Hasan Can <hasancankucuk@yandex.com>
2025-09-02 19:57:54 +0200

    Update vite.config.ts

--- frontend/trace-ui/vite.config.ts
+++ frontend/trace-ui/vite.config.ts
@@ -1,19 +1,21 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    watch: {
+      ignored: ['**/node_modules/**', '**/.git/**']
+    },
     host: true,
     port: 3000
   },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 })
